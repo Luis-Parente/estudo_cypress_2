e8d9ed7c2f0662c08b9b373e56584d9b6fb274ad
Luís Parente <parente.lgsp@outlook.com>
2026-05-02 12:20:09 -0300

    Corrigido cypress config

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -1,10 +1,6 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  env: {
-    email: 'clinica@teste.com',
-    senha: 'Senha123'
-  },
   e2e: {
          setupNodeEvents(on, config) {
       // implement node event listeners here
