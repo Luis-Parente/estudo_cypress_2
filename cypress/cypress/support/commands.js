82fc89d8ed6e61468fdb80700287c53230785097
Luís Parente <parente.lgsp@outlook.com>
2026-05-02 09:43:14 -0300

    Adicionado testes da tela de cadastro de clínica

--- cypress/cypress/support/commands.js
+++ cypress/cypress/support/commands.js
@@ -1,25 +1,18 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('cadastroClínica', (nome, cnpj, email, senha) => {
+    cy.get('[data-test="inputNome"]').type(nome);
+    cy.get('[data-test="inputCNPJ"]').type(cnpj);
+    cy.get('[data-test="inputEmail"]').type(email);
+    cy.get('[data-test="inputSenha"]').type(senha);
+    cy.get('[data-test="inputSenhaVerificada"]').type(senha);
+    cy.get('.sc-bcXHqe').click();
+})
+
+Cypress.Commands.add('cadastroTecnico', (telefone, cep, rua, numero, complemento, estado) => {
+    cy.get('[data-test="inputTelefone"]').type(telefone);
+    cy.get('[data-test="inputCEP"]').type(cep);
+    cy.get('[data-test="inputRua"]').type(rua);
+    cy.get('[data-test="inputNumero"]').type(numero);
+    cy.get('[data-test="inputComplemento"]').type(complemento);
+    cy.get('[data-test="inputEstado"]').type(estado);
+    cy.get('.sc-bcXHqe').click();
+})
